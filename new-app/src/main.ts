da2c605d1fc1b1a81c011ef1ce3c9a8a0c2880ec
Akshay Vadher <akshay@incubyte.co>
2023-12-15 17:05:22 +0530

    Boostrap-ed ngJs inside ng

--- new-app/src/main.ts
+++ new-app/src/main.ts
@@ -1,6 +1,16 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {appConfig} from './app/app.config';
+import {AppComponent} from './app/app.component';
+import {UpgradeModule, setAngularJSGlobal} from '@angular/upgrade/static';
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(err))
+  .then((platformRef) => {
+    console.log('Angular Bootstrap!');
+    const upgrade = platformRef?.injector.get(UpgradeModule) as UpgradeModule;
+    console.log('Injected the UpgradeModule!');
+    // upgrade.bootstrap(document.body, ['phonecatApp'], {
+    //   strictDi: false,
+    // });
+    console.log('AngularJS Bootstrap!');
+  });
